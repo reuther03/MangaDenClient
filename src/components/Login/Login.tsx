ba29c27c1f1170df3f85b78ee92b944638700309
michaldrozd <michaldrozd03@gmail.com>
2025-04-17 02:11:05 +0200

    Added login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,28 @@
-﻿export function Login() {
+﻿import "./Login.css"
+
+export function Login() {
 
     return (
         <>
-            test
+            <div id={"login"}>
+                <form className={"login-form"} action="">
+                    <div className={"input-group"}>
+                        <label>Email</label>
+                        <input className={"login-input"} type="email" required/>
+                    </div>
+                    <div className={"input-group"}>
+                        <label>Password</label>
+                        <input className={"login-input"} type="password" required/>
+                    </div>
+                        <button type="submit" className="btn-primary">
+                            LOGIN
+                        </button>
+
+                    <a href="/forgot" className="forgot-link">
+                        Nie pamiętasz hasła?
+                    </a>
+                </form>
+            </div>
         </>
     );
 }
