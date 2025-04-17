42cbeac6f5b35d64ffc95c5ebb2acd9f3310ab50
michaldrozd <michaldrozd03@gmail.com>
2025-04-17 02:42:58 +0200

    Added login request

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <>
             <TopHeader/>
             <Routes>
-            {/*    <Route path="/" element={<h1>Home</h1>}/>*/}
+                <Route path="/" element={<h1>Home</h1>}/>
             {/*    <Route path="/about" element={<h1>About</h1>}/>*/}
             {/*    <Route path="/services" element={<h1>Services</h1>}/>*/}
             {/*    <Route path="/contact" element={<h1>Contact</h1>}/>*/}
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,26 +1,72 @@
 ﻿import "./Login.css"
+import {useState} from "react";
+import LoginResponse from "./LoginResponse.ts";
+import axios, {AxiosError} from "axios";
+import { useNavigate } from 'react-router-dom';
+
 
 export function Login() {
+    const navigate = useNavigate();
+
+    const url = 'http://localhost:5000/users-module/User/signin';
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [loginResult, setLoginResult] = useState<LoginResponse| null>(null);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post<LoginResponse>(url, { email, password });
+            console.log('Login success:', response.data);
+            setLoginResult(response.data);
+            setErrorMessage(null);
+            localStorage.setItem('token', response.data.value.token)
+            navigate('/', {replace: true})
+        } catch (err: unknown) {
+            if (err instanceof AxiosError) {
+                console.error('Login error:', err.response?.data || err.message);
+                setErrorMessage(err.response?.data.message || 'An error occurred during login.');
+            } else {
+                console.error('Unexpected error:', err);
+                setErrorMessage('An unexpected error occurred.');
+            }
+            setLoginResult(null);
+        }
+    }
+
 
     return (
         <>
             <div id={"login"}>
-                <form className={"login-form"} action="">
+                <form className={"login-form"} onSubmit={handleSubmit}>
                     <div className={"input-group"}>
                         <label>Email</label>
-                        <input className={"login-input"} type="email" required/>
+                        <input
+                            className={"login-input"}
+                            type="email" value={email}
+                            onChange={event =>
+                                setEmail(event.target.value)}
+                            required
+                        />
                     </div>
                     <div className={"input-group"}>
                         <label>Password</label>
-                        <input className={"login-input"} type="password" required/>
+                        <input
+                            className={"login-input"}
+                            type="password"
+                            value={password}
+                            onChange={event =>
+                                setPassword(event.target.value)}
+                            required
+                        />
                     </div>
-                        <button type="submit" className="btn-primary">
-                            LOGIN
-                        </button>
+                        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
 
-                    <a href="/forgot" className="forgot-link">
-                        Nie pamiętasz hasła?
-                    </a>
+                    <button type="submit" className="btn-primary">LOGIN</button>
+                    <a href="/forgot" className="forgot-link">Nie pamiętasz hasła?</a>
                 </form>
             </div>
         </>
